7035b531a2353683836e96a4129e14eb129fb071
Joelozano <128170362+Joelozano@users.noreply.github.com>
2025-08-25 21:05:18 +0000

    Im 30% sure im starting to get it.

--- querying/w3-q.sql
+++ querying/w3-q.sql
@@ -3,15 +3,18 @@
 .mode box
   
 -- changes terminal output from standard output (stdout) to pow.txt file
-.output '| cat >> pow-43.txt'
+.output '| cat >> pow-w3.txt'
   
 -- updates the pow.txt with content in single quotes
-.print 'first query'  
+  
   
 -- SQL command (query) to view all the column contents in the longlist table   
 -- Copy your SQL command below here
-.print 'content'
-  
+.print 'Books less then 200 pages '
+  SELECT "title" FROM "longlist" WHERE "pages" < 200;
+  SELECT COUNT(*) AS "books less then 200" FROM "longlist" WHERE "pages" < 200;
+
+
 -- After each commit comment out the queries written and then continue to write your queries in this part of the file
 
 -- changes back to stdout    
