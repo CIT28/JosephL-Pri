f01c22b5f16571c0f6310c66f7fa1028c0e6f76c
Joelozano <128170362+Joelozano@users.noreply.github.com>
2025-09-03 03:21:23 +0000

    setup done

---
+++ ps0/a-q.sql
@@ -0,0 +1,20 @@
+-- starting the pow workflow
+-- changes terminal output for readability 
+.mode box
+  
+-- changes terminal output from standard output (stdout) to pow.txt file
+.output '| cat >> powa.txt'
+  
+-- updates the pow.txt with content in single quotes
+.print ' '  
+  
+-- SQL command (query) to view all the column contents in the longlist table   
+-- Copy your SQL command below here
+
+    .print 'the use of count and distinct will gives you the full value of how any value you have with out recounting repating values.'
+-- After each commit comment out the queries written and then continue to write your queries in this part of the file
+
+-- changes back to stdout    
+.output stdout  
+
+-- bash command sqlite3 cyberchase.db < a-q.sql
