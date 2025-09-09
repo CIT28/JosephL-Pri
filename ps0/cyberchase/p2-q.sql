0a6f959da958a0c9d1930c13b2ba0b4dbbf9b380
Joelozano <128170362+Joelozano@users.noreply.github.com>
2025-09-09 05:17:13 +0000

    prob 8,9,10 done

---
+++ ps0/cyberchase/p2-q.sql
@@ -0,0 +1,20 @@
+-- starting the pow workflow
+-- changes terminal output for readability 
+.mode box
+  
+-- changes terminal output from standard output (stdout) to pow.txt file
+.output '| cat >> pow-p2.txt'
+  
+-- updates the pow.txt with content in single quotes
+.print 'Couting results  '  
+  
+-- SQL command (query) to view all the column contents in the longlist table   
+-- Copy your SQL command below here
+
+    
+-- After each commit comment out the queries written and then continue to write your queries in this part of the file
+
+-- changes back to stdout    
+.output stdout  
+
+-- bash command sqlite3 cyberchase.db < p2-q.sql
