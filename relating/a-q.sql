d9dba62c5522cc27f139d14214521237ab1d49ea
Joelozano <128170362+Joelozano@users.noreply.github.com>
2025-09-15 20:57:07 +0000

    video 2

--- relating/a-q.sql
+++ relating/a-q.sql
@@ -1,12 +1,16 @@
 .mode box
 .output '| cat >> pow-a.txt'
 -- Queries below here 
-.print '<---->'
 --.tables
 --.schema
-SELECT * FROM "authors" LIMIT 10;
-SELECT * FROM "books" LIMIT 10;
-SELECT * FROM "authored" LIMIT 10;
+--SELECT * FROM "authors" LIMIT 10;
+--SELECT * FROM "books" LIMIT 10;
+--SELECT * FROM "authored" LIMIT 10;
+.print 'Cheon Myeong-kwan'
+SELECT * FROM "books" WHERE "title" = 'Whale';
+SELECT * FROM "authored" WHERE "book_id" = 2;
+SELECT * FROM "authors" WHERE "id" = 13;
+
 .print '<---->'
 -- Queries end here
 .output stdout  
