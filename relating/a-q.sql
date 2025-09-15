66d410271fa889a8746abff52ddd7a336772bd84
Joelozano <128170362+Joelozano@users.noreply.github.com>
2025-09-15 21:23:22 +0000

    video 3

--- relating/a-q.sql
+++ relating/a-q.sql
@@ -6,10 +6,25 @@
 --SELECT * FROM "authors" LIMIT 10;
 --SELECT * FROM "books" LIMIT 10;
 --SELECT * FROM "authored" LIMIT 10;
-.print 'Cheon Myeong-kwan'
-SELECT * FROM "books" WHERE "title" = 'Whale';
-SELECT * FROM "authored" WHERE "book_id" = 2;
-SELECT * FROM "authors" WHERE "id" = 13;
+--.print 'Cheon Myeong-kwan'
+--.print 'Staic Queries'
+--SELECT * FROM "books" WHERE "title" = 'Whale';
+--SELECT * FROM "authored" WHERE "book_id" = 2;
+--SELECT * FROM "authors" WHERE "id" = 13;
+.print 'Subquery'
+--SELECT * FROM "authors" WHERE "id" = (
+--    SELECT "author_id" FROM "authored" WHERE "book_id" = (
+--        SELECT "id" FROM "books" WHERE "title" = 'Whale'
+ --   )
+--
+--);
+.print 'another subquery example'
+SELECT * FROM "authors" WHERE "id" = (
+    SELECT "author_id" FROM "authored" WHERE "book_id" = (
+        SELECT "id" FROM "books" WHERE "title" = 'Time Shelter'
+    )
+);
+
 
 .print '<---->'
 -- Queries end here
