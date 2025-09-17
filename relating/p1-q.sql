ea75db7ebdd3f07e2884eb0627aee078d70a7ddf
Joelozano <128170362+Joelozano@users.noreply.github.com>
2025-09-17 04:00:36 +0000

    1:N subquery

--- relating/p1-q.sql
+++ relating/p1-q.sql
@@ -1,13 +1,22 @@
 .mode box
 .output '| cat >> pow-p1.txt'
   
-.print 'what does this query to'  
- --SELECT "id" FROM "publishers" WHERE "publisher" = 'MacLehose Press';
--- this query is used to just look for the data that is called Maclehose Press.
+.print 'what does this query to  
+ SELECT "id" FROM "publishers" WHERE "publisher" = 'MacLehose Press';
+--this query is used to just look for the data that is called Maclehose Press.'
   
-.print 'what does this query to'  
---SELECT title FROM books WHERE publisher_id = 12;
---This query returns you the answer to what book has the publisher id of 12. 
+.print 'what does this query to 
+SELECT "title" FROM "books" WHERE "publisher_id" = 12;
+--This query returns you the answer to what book has the publisher id of 12.'
+
+.print 'what does this query to
+SELECT title FROM books WHERE publisher_id = ( SELECT id FROM publishers WHERE publisher = 'MacLehose Press');'
+This query combines both the previous queries and the answer that is the same in both tables. 
+
+
+
+
 .output stdout  
+
 
 -- bash command sqlite3 longlist.db < p1-q.sql
