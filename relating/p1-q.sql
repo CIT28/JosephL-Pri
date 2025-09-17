54857edae88039e765388aa443baf9aa2c73bf3e
Joelozano <128170362+Joelozano@users.noreply.github.com>
2025-09-17 02:05:33 +0000

    ERD

--- relating/p1-q.sql
+++ relating/p1-q.sql
@@ -1,21 +1,11 @@
--- starting the pow workflow
--- changes terminal output for readability 
 .mode box
-  
--- changes terminal output from standard output (stdout) to pow.txt file
 .output '| cat >> pow-p1.txt'
   
--- updates the pow.txt with content in single quotes
-.print 'first query'  
-  
--- SQL command (query) to view all the column contents in the longlist table   
--- Copy your SQL command below here
-
+.print ''  
+ 
 
   
--- After each commit comment out the queries written and then continue to write your queries in this part of the file
 
--- changes back to stdout    
 .output stdout  
 
 -- bash command sqlite3 longlist.db < p1-q.sql
