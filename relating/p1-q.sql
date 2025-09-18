9413a786d87f7ea9c754b45a96c37b5d1601f90a
Joelozano <128170362+Joelozano@users.noreply.github.com>
2025-09-18 02:03:36 +0000

    M:N subquery using IN

--- relating/p1-q.sql
+++ relating/p1-q.sql
@@ -23,8 +23,8 @@ it will gives all the rating of the book with an id of 33. But it will return al
 .print 'what does this query to
 SELECT rating FROM ratings WHERE book_id = ( SELECT id FROM books WHERE title = 'In Memory of Memory');
 SELECT AVG(rating) FROM ratings WHERE book_id = ( SELECT id FROM books WHERE title = 'In Memory of Memory');'
- the first will give you the data that the subquery ask for. But the second codenses that list in the average score. 
-.output stdout  
+the first will give you the data that the subquery ask for. But the second codenses that list in the average score. 
+
 
 .print 'what does this query to
 SELECT author_id FROM authored WHERE book_id = ( SELECT id FROM books WHERE title = 'Flights' );'
@@ -35,7 +35,24 @@ SELECT name FROM authors WHERE id =( SELECT author_id FROM authored WHERE book_i
 This query takes the answer from the previous query and used it to find the author of the book. 
 
 .print 'what does this query to
-SELECT name FROM authors WHERE ID =( SELECT author_id FROM authored WHERE book_id = (SELECT id FROM books WHERE title = 'The Birthday Party') );'
+SELECT name FROM authors WHERE ID =( SELECT author_id FROM authored WHERE book_id = (SELECT id FROM books WHERE title = 'The Birthday Party') ); '
 This query using the nesting idea. to find the name of the author of the book flights. Using the books name we used sql to find it name by asking for its book id and id to find the answer.
- 
+
+.print 'what does this query to
+SELECT id FROM authors WHERE name = 'Fernanda Melchor';'
+This is a standard static query that is just asking for the name of the author.
+
+.print 'what does this query to
+SELECT book_id FROM authored WHERE author_id = ( SELECT id FROM authors WHERE name = 'Fernanda Melchor');'
+This nesting query is asking for the book id of the author we asked in the previous query. But it returns two answers and put us into a spot to figure out.
+
+.print 'what does this query to
+ SELECT title FROM books WHERE id IN ( SELECT book_id FROM authored WHERE author_id = (SELECT id FROM authors WHERE name = 'Fernanda Melchor') ) ;'
+This last query uses the IN keyword and helps us with that double answer we recived last query. In is used to say our answer is IN a certain set of values. And so the In pulls what our double answer has in common which is the authors name. 
+
+.output stdout  
+
+
+
+
 -- bash command sqlite3 longlist.db < p1-q.sql
