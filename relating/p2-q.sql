c4f973067d3da4c4a56fa1d5fd97e454e43e1b9c
Joelozano <128170362+Joelozano@users.noreply.github.com>
2025-09-24 05:20:15 +0000

    JOIN Query

--- relating/p2-q.sql
+++ relating/p2-q.sql
@@ -2,7 +2,18 @@
 .output '| cat >> pow-p2.txt'
   
 .print 
+'what does this query to.This query is the regualr query to produce a sent of base results.'
 SELECT * FROM sea_lions;
+SELECT * FROM migrations;
+
+.print 
+'what does this query to this is a query that uses join. And this will pull keys that match and join them in one table'
+SELECT * FROM sea_lions 
+JOIN migrations ON migrations.id = sea_lions.id;
+'what does this query to
+
+
+
 .output stdout  
 
 
