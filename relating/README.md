2b5db324366edfdefeddcc9b8828ee545413d01f
Joelozano <128170362+Joelozano@users.noreply.github.com>
2025-09-24 06:58:12 +0000

    LEFT, RIGHT, FULL and NATURAL JOIN query

--- relating/README.md
+++ relating/README.md
@@ -15,4 +15,18 @@ Join is used to combine multiple tables together and get a specfic data set. It 
 ##'Inner JOIN'
 inner join will only return colums where they match. for example if two tables have id that match with another table. 
 
-## 'LEFT, RIGHT and FULL JOIN'
+## 'LEFT, RIGHT and FULL JOIN OUTER NATURAL '
+A left join will prioritize data to the left side of the table.
+A right join will priortize date to the right side of the table.
+Both the right and left will also include any data that over laps as well. 
+A LEFT OUTER JOIN will show all the data on the left sid of the table even if there is a null.
+A RIGHT OUTER JOIN will show all the dat on the left side of the table even if there is a null 
+A FULL OUTER JOIN  will show evrything on the will include all nulls.
+A natural join cleans up your table it git rid of any data that has null values and doesnt repeat row. Making the table look nicer. 
+
+
+"How is left and right determined?
+the table that comes first before your join command. 
+
+do we get back a separate table?
+We get more of a tempory table that only last as long as we have the query in use. 
--- relating/p2-q.sql
+++ relating/p2-q.sql
@@ -1,27 +1,27 @@
 .mode box
 .output '| cat >> pow-p2.txt'
   
-.print
-'what does this query to.This query is the regualr query to produce a sent of base results.'
-SELECT * FROM sea_lions;
-SELECT * FROM migrations;
+-- .print 'what does this query to.This query is the regualr query to produce a sent of base results'
+-- SELECT * FROM sea_lions;
+-- SELECT * FROM migrations;
 
-.print
-' what does this query to this is a query that uses join. And this will pull keys that match and join them in one table'
-SELECT * FROM sea_lions 
-JOIN migrations ON migrations.id = sea_lions.id;
+-- .print 'What does this query do? It brings everything from the left side of the table.'
+-- SELECT * FROM sea_lions LEFT JOIN migrations ON migrations.id = sea_lions.id;
 
-.print
-'what does this query do it bring everything from the left side of the table'
-SELECT * FROM sea_lions LEFT JOIN migrations ON migrations.id = sea_lions.id;
+-- .print 'what does this query do? it bring everything from the left side of the table.'
+-- SELECT * FROM sea_lions LEFT JOIN migrations ON migrations.id = sea_lions.id;
 
-.print
-'Whatdoes this query do?t brings evrything to the ridght side of the table and heven brings the extra.'
-SELECT * FROM sea_lions RIGHT JOIN migrations ON migrations.id = sea_lions.id;
+-- .print 'Whatdoes this query do?t brings evrything to the ridght side of the table and heven brings the extra.'
+-- SELECT * FROM sea_lions RIGHT JOIN migrations ON migrations.id = sea_lions.id;
 
-.print 'What does this query do? It brings everything from the left side of the table.'
-SELECT * FROM sea_lions LEFT JOIN migrations ON migrations.id = sea_lions.id;
+-- .print 'What does this query do? It brings everything from the left side of the table.'
+-- SELECT * FROM sea_lions LEFT JOIN migrations ON migrations.id = sea_lions.id;
 
+.print 'What does this query do? It combines both tables including the nulls'
+SELECT * FROM sea_lions FULL JOIN migrations on migrations.id = sea_lions.id;
+
+.print 'What does this query do?The natural join cleans up the table.It gets rid of any repeating rows'
+SELECT * FROM sea_lions NATURAL JOIN migrations;
 .output stdout  
 
 
