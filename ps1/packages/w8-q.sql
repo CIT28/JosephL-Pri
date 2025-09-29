65eabe38501890848899b9f98ff905a303e5311f
Joelozano <128170362+Joelozano@users.noreply.github.com>
2025-09-29 20:48:16 +0000

    new database

---
+++ ps1/packages/w8-q.sql
@@ -0,0 +1,14 @@
+.mode box
+.output '| cat >> pow-ds.txt'
+
+-- SELECT * FROM addresses LEFT JOIN packages ON packages.from_address_id = addresses.id;
+SELECT * FROM addresses LEFT JOIN packages ON packages.from_address_id = addresses.id LIMIT 10;
+.print "I wanted to use join and see  if i understood using it."
+
+SELECT * FROM addresses LIMIT 10;
+SELECT * FROM drivers LIMIT 10;
+SELECT * FROM packages LIMIT 10;
+SELECT * FROM scans LIMIT 10;
+.print "I used a simple * to get the first set of 10 from the tables."
+
+-- bash command sqlite3 packages.db < w8-q.sql
