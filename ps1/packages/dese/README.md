bf48776219d5e177835a7d97e3f912ab9bda78f0
Joelozano <128170362+Joelozano@users.noreply.github.com>
2025-10-14 03:48:00 +0000

    2. query, pow and README.md complete

--- ps1/packages/dese/README.md
+++ ps1/packages/dese/README.md
@@ -5,6 +5,10 @@
 SELECT COUNT (*) AS "Pub School COUNT" FROM (SELECT "name", "city", "type" FROM "schools");
 
 ## "Query 2"
+SELECT "name" FROM "districts" WHERE "name" LIKE '%(non-op)' ;
+
+SELECT COUNT("name") AS "Non-op Schools" FROM "districts" WHERE "name" LIKE '%(non-op)' ;
+
 ## "Query 3"
 ## "Query 4"
 ## "Query 5"
--- ps1/packages/dese/dese-q.sql
+++ ps1/packages/dese/dese-q.sql
@@ -1,7 +1,7 @@
 .mode box
 .output '| cat >> pow-dese.txt'
 --Queries below here
-.print "<====>"
+-- .print "<====>"
 -- .tables
 -- .schema
 -- SELECT * FROM "districts" LIMIT 10;
@@ -10,19 +10,25 @@
 -- SELECT * FROM "expenditures" LIMIT 10;
 --  SELECT * FROM "schools" LIMIT 10;
 
-.print "Query 1"
-.print "find the names and cities of all public schools in"
+-- .print "Query 1"
+-- .print "find the names and cities of all public schools in"
 
-SELECT "name", "city", "type" 
-FROM "schools" 
-WHERE "type" = 'Public school';
+-- SELECT "name", "city", "type" 
+-- FROM "schools" 
+-- WHERE "type" = 'Public school';
 
-.print "Count for query 1"
-.print "count Results"
-SELECT COUNT (*) AS "Pub School COUNT" FROM 
-    (SELECT "name", "city", "type" 
-    FROM "schools"
-     WHERE "type" LIKE '%Public School%'
-);
+.print "Count for query 2"
+-- .print "count Results"
+-- SELECT COUNT (*) AS "Pub School COUNT" FROM 
+--     (SELECT "name", "city", "type" 
+--     FROM "schools"
+--      WHERE "type" LIKE '%Public School%'
+-- );
+-- SELECT "name" FROM "districts" WHERE "name" LIKE '%(non-op)' ;
+
+-- SELECT COUNT("name") AS "Non-op Schools" FROM "districts" WHERE "name" LIKE '%(non-op)' ;
+
+
 
 -- bash command sqlite3 dese.db < dese-q.sql
+
