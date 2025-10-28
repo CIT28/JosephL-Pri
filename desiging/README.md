cc04c5e5dda414e237bbdb96d9c175985117cd22
Joelozano <128170362+Joelozano@users.noreply.github.com>
2025-10-28 01:18:55 +0000

    schema for diffrent longlist.db

---
+++ desiging/README.md
@@ -0,0 +1,4 @@
+# Week 12 Attendance
+-Answer how can we reference the different version of the longlist.db without copying it to the designing folder?
+-My answer 
+You would use the directory to connect longlist to this current directory. You change directory.
--- desiging/w12-q.sql
+++ desiging/w12-q.sql
@@ -1,6 +1,14 @@
 .mode box
 .output '| cat >> pow-a.txt'
 
-.print 
+.print '<--------->'
+-- .print 'Schema for single table version of longlist.db'
+.print 'Schema for multiple table version of longlist.db'
+.schema
 
--- bash command sqlite3 moneyball.db < w12-q.sql
+
+
+
+.output stdout
+
+-- bash command sqlite3 ../relating/longlist.db < w12-q.sql
