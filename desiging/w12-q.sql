b35a969d74d0ad5565cec28e3cb99dcb2904a2ae
Joelozano <128170362+Joelozano@users.noreply.github.com>
2025-10-28 01:29:30 +0000

    creating the cit 28.db

--- desiging/w12-q.sql
+++ desiging/w12-q.sql
@@ -5,10 +5,15 @@
 -- .print 'Schema for single table version of longlist.db'
 .print 'Schema for multiple table version of longlist.db'
 .schema
+-- CREATE TABLE "students"(
+--     "id",
+--     "firstname",
+--     "lastname
 
+-- );
 
 
 
 .output stdout
 
--- bash command sqlite3 ../relating/longlist.db < w12-q.sql
+-- bash command sqlite3 cit28.db < w12-q.sql
