be52215e8bff65c98225f263789c27d6a634c9e1
Joelozano <128170362+Joelozano@users.noreply.github.com>
2025-10-30 03:11:55 +0000

    CREATE TABLE

--- desiging/p1-q.sql
+++ desiging/p1-q.sql
@@ -7,6 +7,16 @@
 -- (x1...> "id",
 -- (x1...> "name");
 
+--  CREATE TABLE "stations" (
+-- (x1...>     "id",
+-- (x1...>     "name",
+-- (x1...>     "line" );
+
+--  CREATE TABLE "visits"
+--    ...> (
+-- (x1...>     "rider_id",
+-- (x1...>     "station_id"
+-- (x1...> );
 .output stdout
 
 -- bash command sqlite3 mbta.db < p1-q.sql
